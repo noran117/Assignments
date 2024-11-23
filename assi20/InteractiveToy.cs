8df4826c07f0492cbb964f2d122ad7e4eb4797e0
noran <noran@NORAN-MANAMA>
2024-11-23 15:41:30 +0200

    assi20

--- assi20/InteractiveToy.cs
+++ assi20/InteractiveToy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class InteractiveToy : MonoBehaviour
 {
 
     public float moveSpeed = 5f; 
