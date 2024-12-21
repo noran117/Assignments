b2b9a16b29083e0826596757b9f47c9d2e1488f1
noran <noran@NORAN-MANAMA>
2024-12-21 10:43:38 +0200

    Assignment27

--- assi27/callByReference.cs
+++ assi27/callByReference.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace Assignment27
 {
     public class callByReference : MonoBehaviour
@@ -12,33 +13,35 @@ namespace Assignment27
         int num1 = 5;
         int num2 = 10;
 
-        Debug.Log("Before callByValue"+num1);
+        Debug.Log("Before calling by value "+num1);
         callByValue(num1);
-        Debug.Log("After callByValue" + num1);
+        Debug.Log("After calling by value " + num1);
 
-        Debug.Log("Before callByreference" + num2);
+        Debug.Log("Before calling by reference " + num2);
         callByreference(ref num2);
-        Debug.Log("After callByreference" + num2);
+        Debug.Log("After calling by reference " + num2);
 
         int num3;
         usingOut(out num3);
-        Debug.Log("After usingOut" + num3);
+        Debug.Log("After usingOut " + num3);
 
-    }
-
-    private void usingOut(out int num)
-    {
-        num = 20;
-    }
-
-    private void callByreference(ref int num)
-    {
-        num += 10;
     }
 
     private void callByValue(int num)
     {
         num += 10;
     }
+   
+    private void callByreference(ref int num)
+    {
+        num += 10;
+    }
+     private void usingOut(out int num)
+    {
+        num = 20;
+    }
+
+
+    
 }
 }
